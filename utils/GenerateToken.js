a150d266b6b8ca1b9e9edef95f9a002890e669a3
Divyarajsinhchavda <chavdadivyaraj07@gmail.com>
2026-07-15 23:44:15 +0530

    fix: set default jwt token expiration fallback

--- utils/GenerateToken.js
+++ utils/GenerateToken.js
@@ -2,5 +2,12 @@ require('dotenv').config()
 const jwt=require('jsonwebtoken')
 
 exports.generateToken=(payload,passwordReset=false)=>{
-    return jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:passwordReset?process.env.PASSWORD_RESET_TOKEN_EXPIRATION:process.env.LOGIN_TOKEN_EXPIRATION})
+    const exp = passwordReset ? process.env.PASSWORD_RESET_TOKEN_EXPIRATION : process.env.LOGIN_TOKEN_EXPIRATION;
+    const options = {};
+    if (exp) {
+        options.expiresIn = exp;
+    } else {
+        options.expiresIn = passwordReset ? '1h' : '30d';
+    }
+    return jwt.sign(payload,process.env.SECRET_KEY,options)
 }
